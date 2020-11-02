42ec522876cd6723702b788f39d09fdfbe7e786c
Leo <leonardosdantas@protonmail.com>
2020-11-02 11:38:41 -0300

    Algumas mudanças. Exemplo mais complexo.

--- metropolis.py
+++ metropolis.py
@@ -10,25 +10,22 @@ import numpy as np
 # ------- Parâmetros Iniciais -------
 
 # Definir Distribuição Estacionária Desejada
-pi = np.array([0.1, 0.2, 0.7])
+pi = np.array([0.1, 0.2, 0.6, 0.1])
 
 # Definir Cadeia de Transição Proposta
-transicao_proposta = np.array( [ [0.1, 0.8, 0.1], [0.4, 0.3, 0.3], [0.9, 0.1, 0] ] )
+transicao_proposta = np.array( [ [0.7, 0.1, 0.1, 0.1], [0.2, 0.3, 0.3, 0.2], [0.1, 0.2, 0.3, 0.4], [ 0.0, 0.3, 0.7, 0.0]] )
 n_estados = transicao_proposta.shape[0]
 
 # ------ Método Computacional -------
 
 # Escolher Estado Inicial
 estado = 2
-cadeia = []
+cadeia = [estado]
 
 # Definir Número de Iterações
-num_iter = 10000
+num_iter = 100000
 
 for i in range(0, num_iter):
-    # Armazenar Estado
-    cadeia.append(estado)
-
     # Escolher Candidato para Transição com base em Cadeia Proposta
     candidato = np.random.choice( n_estados, p=transicao_proposta[estado, :] )
 
@@ -43,8 +40,14 @@ for i in range(0, num_iter):
         # Aceitar com Probabilidade Alfa
         if np.random.rand() < alfa:
             estado = candidato
+    
+    # Armazenar Estado
+    cadeia.append(estado)
 
 
+# Calcular Frequências de Estados na Cadeia Resultante
 _, contagem = np.unique(cadeia, return_counts=True)
 frequencia = contagem / len(cadeia)
+
+# Exibir Resultados
 print(frequencia)
